ae4f017788c6eb4832a7ecb211c380be8c6fe27d
wurtzii <wurtzii05@gmail.com>
2024-09-02 23:32:38 -0500

    bluetooth support

--- laptop/nixos/configuration.nix
+++ laptop/nixos/configuration.nix
@@ -5,100 +5,119 @@
 { inputs, lib, config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+    imports =
+        [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+        ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-	
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+# Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  nixpkgs = {
-    overlays = [
-    ];
-    config = {
-      allowUnfree = true;
-    };
-  };
+    networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  nix = let
-      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+        nixpkgs = {
+            overlays = [
+            ];
+            config = {
+                allowUnfree = true;
+            };
+        };
+
+    nix = let
+        flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
     in {
-      settings = {
-        experimental-features = "nix-command flakes";
-	flake-registry = "";
-	nix-path = config.nix.nixPath;
-      };
+        settings = {
+            experimental-features = "nix-command flakes";
+            flake-registry = "";
+            nix-path = config.nix.nixPath;
+        };
 
-	channel.enable = false;
-	registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
-	nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
-  };
+        channel.enable = false;
+        registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+        nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+    };
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+# Enable networking
+    networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Winnipeg";
+    hardware.bluetooth.enable = true;
+    hardware.bluetooth.powerOnBoot = true;
+    hardware.enableAllFirmware = true;
+    hardware.bluetooth.settings = {
+	    General = {
+		    Enable = "Source,Sink,Media,Socket";
+	    };
+    };
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_CA.UTF-8";
+# rtkit is optional but recommended
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+#jack.enable = true;
+    };
 
-  programs.sway.enable = true;
+# Set your time zone.
+    time.timeZone = "America/Winnipeg";
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+# Select internationalisation properties.
+    i18n.defaultLocale = "en_CA.UTF-8";
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.gabriel = {
-    isNormalUser = true;
-    description = "gabriel";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
+    programs.sway.enable = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  wget
-  ];
+# Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+# Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.gabriel = {
+        isNormalUser = true;
+        description = "gabriel";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [];
+    };
 
-  # List services that you want to enable:
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+    environment.systemPackages = with pkgs; [
+#  wget
+    ];
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+# List services that you want to enable:
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.05"; # Did you read the comment?
 
 }
