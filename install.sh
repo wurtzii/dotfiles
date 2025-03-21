c069d996b2845273a124604517f602ddfcef911c
gabriel <wurtzii05@gmail.com>
2025-03-21 11:53:51 -0500

    add nwg-panel config

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ln -s /home/gabriel/workspace/wurtzii/dotfiles/nvim /home/gabriel/.config/nvim
-ln -s /home/gabriel/workspace/wurtzii/dotfiles/sway /home/gabriel/.config/sway
-ln -s /home/gabriel/workspace/wurtzii/dotfiles/waybar /home/gabriel/.config/waybar
-
+# ln -s /home/gabriel/workspace/wurtzii/dotfiles/nvim /home/gabriel/.config/nvim
+# ln -s /home/gabriel/workspace/wurtzii/dotfiles/sway /home/gabriel/.config/sway
+# ln -s /home/gabriel/workspace/wurtzii/dotfiles/waybar /home/gabriel/.config/waybar
+ln -s /home/gabriel/workspace/wurtzii/dotfiles/nwg-panel /home/gabriel/.config/nwg-panel
+ln -s /home/gabriel/workspace/wurtzii/dotfiles/nwg-look /home/gabriel/.config/nwg-look
